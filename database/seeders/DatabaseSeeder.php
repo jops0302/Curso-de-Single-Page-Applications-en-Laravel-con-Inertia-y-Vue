aa02b2f35b0887f9d034846742a1d0b088224930
OrlandoSoto <jops0302@gmail.com>
2021-02-18 14:21:36 -0500

    proyecto terminado

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Note;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        User::factory()->create(([
+            'email' => 'i@admin.com',
+            'password' => bcrypt('123456')
+        ]));
+
+        Note::factory(30)->create();
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::view('/', 'index');
+
+Route::get('dashboard', [\App\Http\Controllers\PageController::class, 'dashboard'])
+    ->middleware('auth:sanctum')
+    ->name('dashboard');
+
+Route::resource('notes', \App\Http\Controllers\NoteController::class)
+    ->middleware('auth:sanctum');
+    
+
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
+// Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->name('dashboard');
